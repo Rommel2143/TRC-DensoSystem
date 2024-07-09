c1f06a18948421384aeee4175db817e847eb9561
PTI-001 <itstaff@trc-ph.com>
2024-07-09 15:55:47 +0800

    21

--- TRC-DensoSystem/INFG/INFG.vb
+++ TRC-DensoSystem/INFG/INFG.vb
@@ -22,94 +22,68 @@ Public Class INFG
     Private Sub boxtype_SelectedIndexChanged(sender As Object, e As EventArgs) Handles boxtype.SelectedIndexChanged
         txtqr.Enabled = True
         txtqr.Focus()
-
     End Sub
 
     Private Sub txtqr_KeyDown(sender As Object, e As KeyEventArgs) Handles txtqr.KeyDown
         If e.KeyCode = Keys.Enter Then
             Dim qrlenght As Integer
             Dim qrcode As String = txtqr.Text
-
+            qrlenght = qrcode.Length
             If boxtype.Text = "DMTN" Then
                 txtcode.Text = qrcode.Substring(0, 14)
                 txtcolor1.Text = qrcode.Substring(0, 17)
                 txtqty.Text = qrcode.Substring(14, 3)
                 txtlot.Text = qrcode.Substring(qrcode.Length - 12)
-                colorcode()
-                Dim prodd As String = txtlot.Text
-                Dim proddate As String = prodd.Substring(0, 6)
-                Dim year As Integer = Integer.Parse(proddate.Substring(0, 2))
-                Dim month As Integer = Integer.Parse(proddate.Substring(2, 2))
-                Dim day As Integer = Integer.Parse(proddate.Substring(4, 2))
-                Dim prodf As New DateTime(2000 + year, month, day)
-                txtproddate.Text = prodf.ToString("yyyy-MM-dd")
-                Dim ddate As String = prodf.ToString("yyyy-MM-dd")
-                con.Close()
-                con.Open()
-                Dim cmd As New MySqlCommand("Select * FROM `denso_fg_scan` where `qrcode` = '" & txtqr.Text & "'", con)
-                dr = cmd.ExecuteReader
-                If dr.Read = True Then
-                    labelerror2.Visible = True 'DUPLICATE
-                Else
-                    labelerror2.Visible = False
-                    con.Close()
-                    con.Open()
-                    Dim cmd1 As New MySqlCommand("Select * FROM `denso_fg_masterlist` where `partno` = '" & txtcode.Text & "' and `qrtype`='" & boxtype.Text & "'", con)
-                    dr = cmd1.ExecuteReader
-                    If dr.Read = True Then
-                        labelerror1.Visible = False
-                        txtcustomer.Text = (dr.GetString("customerno"))
-                        txtmodel.Text = (dr.GetString("model"))
-                        txtname.Text = (dr.GetString("partname"))
-                        txthide1.Text = (dr.GetInt32("qty"))
-                        Dim x As Integer
-                        x = Val(txthide1.Text)
-                        con.Close()
-                        updates("Update `denso_fg_masterlist` set `qty`='" & Val(txtqty.Text) + x & "' where `partno` = '" & txtcode.Text & "' and `qrtype`='" & boxtype.Text & "' and `customerno`='" & txtcustomer.Text & "'")
-                        con.Close()
-                        insertitem("Insert into `denso_fg_scan` (`status`,`datein`,`shift`,`operator`,`type`,`qrcode`,`partno`,`customerno`,`model`,`color`,`quantity`,`lotnumber`,`proddate`)
-                                    values ('" & txthide.Text & "','" & datedb & "','" & boxshift.Text & "','" & txtoperator.Text & "','" & boxtype.Text & "','" & txtqr.Text & "','" & txtcode.Text & "','" & txtcustomer.Text & "','" & txtmodel.Text & "','" & txtcolor.Text & "','" & txtqty.Text & "','" & txtlot.Text & "','" & ddate & "')")
-                    Else
-                        labelerror1.Visible = True
-
-                    End If
-
-                End If
+                saveupdate()
             ElseIf boxtype.Text = "INTELLI IV" Then
-                qrlenght = qrcode.Length
-
                 Select Case qrlenght
                     Case 23
                         txtcode.Text = qrcode.Substring(0, 10)
                         txtcolor1.Text = qrcode.Substring(0, 12)
                         txtqty.Text = qrcode.Substring(10, 2)
                         txtlot.Text = qrcode.Substring(qrcode.Length - 11)
-                        colorcode()
-                    Case 26
+                        saveupdate()
+                    Case 26 To 27
                         txtcode.Text = qrcode.Substring(0, 10)
                         txtcolor1.Text = qrcode.Substring(0, 12)
                         txtqty.Text = qrcode.Substring(10, 2)
                         txtlot.Text = qrcode.Substring(qrcode.Length - 12)
-                        colorcode()
-                    Case 27
-                        txtcode.Text = qrcode.Substring(0, 10)
-                        txtcolor1.Text = qrcode.Substring(0, 12)
-                        txtqty.Text = qrcode.Substring(10, 2)
-                        txtlot.Text = qrcode.Substring(qrcode.Length - 12)
-                        colorcode()
+                        saveupdate()
                     Case 31
                         txtcode.Text = qrcode.Substring(0, 14)
                         txtcolor1.Text = qrcode.Substring(0, 16)
                         txtqty.Text = qrcode.Substring(14, 2)
                         txtlot.Text = qrcode.Substring(qrcode.Length - 12)
-                        colorcode()
-
+                        saveupdate()
                 End Select
-
-
+            ElseIf boxtype.Text = "20CY" Then
+                Select Case qrlenght
+                    Case 24 To 28
+                        txtcode.Text = qrcode.Substring(0, 10)
+                        txtcolor1.Text = qrcode.Substring(0, 12)
+                        txtqty.Text = qrcode.Substring(10, 2)
+                        txtlot.Text = qrcode.Substring(qrcode.Length - 12)
+                        saveupdate()
+                    Case 31
+                        txtcode.Text = qrcode.Substring(0, 14)
+                        txtcolor1.Text = qrcode.Substring(0, 16)
+                        txtqty.Text = qrcode.Substring(14, 2)
+                        txtlot.Text = qrcode.Substring(qrcode.Length - 12)
+                        saveupdate()
+                End Select
+            ElseIf boxtype.Text = "VT" Then
+                txtcode.Text = qrcode.Substring(0, 10)
+                txtcolor1.Text = qrcode.Substring(0, 12)
+                txtqty.Text = qrcode.Substring(10, 2)
+                txtlot.Text = qrcode.Substring(qrcode.Length - 11)
+                saveupdate()
+            ElseIf boxtype.Text = "2T" Then
+                txtcode.Text = qrcode.Substring(0, 10)
+                txtcolor1.Text = qrcode.Substring(0, 12)
+                txtqty.Text = qrcode.Substring(10, 2)
+                txtlot.Text = qrcode.Substring(qrcode.Length - 11)
+                saveupdate()
             Else
-
-
                 labelerror1.Visible = True 'INVALID QR
             End If
 
@@ -130,16 +104,56 @@ Public Class INFG
         txtmodel.Text = Nothing
         txthide1.Text = Nothing
         txtcolor.Text = Nothing
+        txtcolor1.Text = Nothing
+        txthide2.Text = Nothing
     End Sub
-    Public Sub colorcode()
+
+    Public Sub saveupdate()
         Dim sn As String = txtqr.Text
         Dim fpart As String = txtcolor1.Text
         Dim lpart As String = txtlot.Text
         Dim rpart As String = sn.Replace(fpart, "").Replace(lpart, "")
         txtcolor.Text = rpart
+        Dim prodd As String = txtlot.Text
+        Dim proddate As String = prodd.Substring(0, 6)
+        Dim year As Integer = Integer.Parse(proddate.Substring(0, 2))
+        Dim month As Integer = Integer.Parse(proddate.Substring(2, 2))
+        Dim day As Integer = Integer.Parse(proddate.Substring(4, 2))
+        Dim prodf As New DateTime(2000 + year, month, day)
+        txtproddate.Text = prodf.ToString("yyyy-MM-dd")
+        Dim ddate As String = prodf.ToString("yyyy-MM-dd")
+        con.Close()
+        con.Open()
+        Dim cmd As New MySqlCommand("Select * FROM `denso_fg_scan` where `qrcode` = '" & txtqr.Text & "'", con)
+        dr = cmd.ExecuteReader
+        If dr.Read = True Then
+            labelerror2.Visible = True 'DUPLICATE
+        Else
+            labelerror2.Visible = False
+            con.Close()
+            con.Open()
+            Dim cmd1 As New MySqlCommand("Select * FROM `denso_fg_masterlist` where `partno` = '" & txtcode.Text & "' and `qrtype`='" & boxtype.Text & "'", con)
+            dr = cmd1.ExecuteReader
+            If dr.Read = True Then
+                labelerror1.Visible = False
+                txtcustomer.Text = (dr.GetString("customerno"))
+                txtmodel.Text = (dr.GetString("model"))
+                txtname.Text = (dr.GetString("partname"))
+                txthide1.Text = (dr.GetInt32("qty"))
+                txthide2.Text = (dr.GetString("color"))
+                Dim x As Integer
+                x = Val(txthide1.Text)
+                con.Close()
+                updates("Update `denso_fg_masterlist` set `qty`='" & Val(txtqty.Text) + x & "' where `partno` = '" & txtcode.Text & "' and `qrtype`='" & boxtype.Text & "' and `customerno`='" & txtcustomer.Text & "'and `color`='" & txthide2.Text & "'")
+                con.Close()
+                insertitem("Insert into `denso_fg_scan` (`status`,`datein`,`shift`,`operator`,`type`,`qrcode`,`partno`,`customerno`,`model`,`color`,`quantity`,`lotnumber`,`proddate`)
+                                    values ('" & txthide.Text & "','" & datedb & "','" & boxshift.Text & "','" & txtoperator.Text & "','" & boxtype.Text & "','" & txtqr.Text & "','" & txtcode.Text & "','" & txtcustomer.Text & "','" & txtmodel.Text & "','" & txtcolor.Text & "','" & txtqty.Text & "','" & txtlot.Text & "','" & ddate & "')")
+            Else
+                labelerror1.Visible = True
 
+            End If
 
-
+        End If
     End Sub
     Public Sub reloadgrid()
         Try
@@ -148,14 +162,5 @@ Public Class INFG
             MessageBox.Show(ex.Message)
         End Try
     End Sub
-    Private Sub txtlot_TextChanged(sender As Object, e As EventArgs) Handles txtlot.TextChanged
 
-    End Sub
-    Public Sub save1()
-
-    End Sub
-
-    Private Sub txtcolor_TextChanged(sender As Object, e As EventArgs) Handles txtcolor.TextChanged
-
-    End Sub
 End Class
